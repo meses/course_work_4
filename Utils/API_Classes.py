8ba185f5cc7c3cf137217808aaab37126d127378
Artem <akirillov@itps-russia.ru>
2023-04-07 01:53:53 +0500

    Усовершенствован метод получения данных с hh

--- Utils/API_Classes.py
+++ Utils/API_Classes.py
@@ -1,6 +1,7 @@
 """Работа с API"""
 import requests
 import json
+from Utils.utils import set_correct_salary_hh, write_to_json
 
 
 class Main_API:
@@ -12,45 +13,31 @@ class HeadHunter(Main_API):
         self.url = f'https://api.hh.ru/vacancies'
 
 
-    def get_requests(self):
-        params = {
-            'User-Agent': 'Mozilla/5.0', # Должен быть агент
-            'text': 'Водитель', # Текст фильтра по вакансии
-            'area': '113', # Указание области (113 - Россия)
-            'per_page': 5, # Кол-во вакансий на одной странице
-            'page': 0 # Индекс страницы
-        }
-        response = requests.get(self.url, params)
-        res = response.json()
-        self.write_to_json('data.json', res)
-        '''
+    def get_requests(self, vacancy_name:str = 'Разработчик'):
+        """Функция для получения вакансий с HH с заданным поисковым запосом"""
+        data = {'items':[]}
         for i in range(1, 11):
-            response = requests.get(self.url, params) #params={'User-Agent': 'Mozilla/5.0', 'area': '113', 'per_page': '100', 'page': str(i)})
-            if i == 1:
-                res = response.json()
-            else:
-                res['items'].append(response.json()['items'][0])
-        self.write_to_json('data.json', res)
-        '''
-
-    @staticmethod
-    def write_to_json(filemname: str, data: dict):
-        """Метод записи данных в json"""
-        with open(filemname, 'w', encoding='UTF-8') as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-
-    def get_info_from_json(self):
-        """Выводит информацию о вакансиях из файла"""
-        filename = 'data.json'
-        with open(filename, 'r', encoding='UTF-8') as file:
-            data = json.load(file)
+            response = requests.get(self.url, params = {'User-Agent': 'Mozilla/5.0', # Должен быть агент
+                                                        'text': f'{vacancy_name}', # Текст фильтра по вакансии
+                                                        'area': '113', # Указание области (113 - Россия)
+                                                        'per_page': 20, # Кол-во вакансий на одной странице
+                                                        'page': str(i) # Индекс страницы
+                                                        })
+            for j in response.json()['items']:
+                item_dict = {}
+                item_dict['name'] = j['name']
+                item_dict['salary'] = set_correct_salary_hh(j['salary'])
+                item_dict['company_name'] = j['employer']['name']
+                item_dict['url'] = j['url']
+                data['items'].append(item_dict)
         return data
+        #self.write_to_json('data.json', data)
 
 
 class SuperJob(Main_API):
     pass
 
-test_hh = HeadHunter()
+#test_hh = HeadHunter().get_requests()
 #test_hh.get_requests()
 #print(test_hh.get_info_from_json()['items'])
 #vacansies = test_hh.get_info_from_json()['items']
