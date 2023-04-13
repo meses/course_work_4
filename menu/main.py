9ff345c70f66f3fb49dcbf1cc5a8d172f135ff9d
Artem <akirillov@itps-russia.ru>
2023-04-13 23:08:43 +0500

    Улучшение логики основной программы

--- menu/main.py
+++ menu/main.py
@@ -4,35 +4,38 @@ from Utils.Vacansy import Vacancy, top_vacancies, show_vacancies
 from Utils.utils import file_or_new_request, clear_json, get_filtered_vacancies
 
 filename = '../Utils/data.json'
-print('Привет!')
 
-user_platform = file_or_new_request(filename)
-filter_word = input(f'Введите название вакансии для поиска: ')
+def main():
+    print('Привет!')
 
-if user_platform != '0':
-    clear_json(filename)
+    user_platform = file_or_new_request(filename)
+    filter_word = input(f'Введите название вакансии для поиска: ')
 
-filtered_vacancies = get_filtered_vacancies(user_platform, filter_word, filename)
+    if user_platform != '0':
+        clear_json(filename)
 
+    filtered_vacancies = get_filtered_vacancies(user_platform, filter_word, filename)
 
-vac_objs = [Vacancy(i['name'], i['salary'], i['company_name'], i['url']) for i in filtered_vacancies] # Создаёт экземпляры класса Vacancy из всего списка вакансий, полученных из файла
+    if len(filtered_vacancies) == 0:
+        print('По вашему запросу ничего не нашлось')
+    else:
+        vac_objs = [Vacancy(i['name'], i['salary'], i['company_name'], i['url']) for i in
+                    filtered_vacancies]  # Создаёт экземпляры класса Vacancy из всего списка вакансий, полученных из файла
+        print(f'Всего вакансий нашлось: {len(vac_objs)}')
+        is_top = input('Вывести топ вакансий по зарплате?\n1 - Да\n2 - Нет\n')
+        if is_top == '1':
+            count_vacancies = int(input('Сколько вакансий показать? '))
+            top = top_vacancies(vac_objs, count_vacancies)
+            show_vacancies(top)
 
-if len(vac_objs) == 0:
-    print('По вашему запросу ничего не нашлось')
-else:
-    print(f'Всего вакансий нашлось: {len(vac_objs)}')
-    is_top = input('Вывести топ вакансий по зарплате?\n1 - Да\n2 - Нет\n')
-    if is_top == '1':
-        count_vacancies = int(input('Сколько вакансий показать? '))
-        top_vacancies = top_vacancies(vac_objs, count_vacancies)
-        show_vacancies(top_vacancies)
+        elif is_top == '2':
+            is_all_vacancies = input(f'Тогда может вывести все найденные вакансии?\n1 - Да\n2 - Нет\n')
+            if is_all_vacancies == '1':
+                show_vacancies(vac_objs)
 
-    elif is_top == '2':
-        is_all_vacancies = input(f'Тогда может вывести все найденные вакансии?\n1 - Да\n2 - Нет\n')
-        if is_all_vacancies == '1':
-            show_vacancies(vac_objs)
+    print('Спасибо, приходите ещё!')
 
-print('Спасибо, приходите ещё!')
-
+if __name__=="__main__":
+    main()
 
 
